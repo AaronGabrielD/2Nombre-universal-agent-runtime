bbb29cb7bc17cf6c1335bcc4d8074d464751084b
AaronGabrielD <aarongdaishonin@gmail.com>
2026-09-08 15:52:21 -0400

    M06: remove accidental main write

--- app/workers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Worker factory and dependency-aware dispatcher."""
-
-from .models import DispatchBatch, WorkerInstance
-from .service import WorkerDispatchError, WorkerFactory, WorkerDispatcher
-
-__all__ = ["DispatchBatch", "WorkerDispatchError", "WorkerDispatcher", "WorkerFactory", "WorkerInstance"]
