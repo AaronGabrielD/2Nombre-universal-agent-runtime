ace492eab1c8ead415fa174da65c36389fed0726
AaronGabrielD <aarongdaishonin@gmail.com>
2026-09-09 08:05:28 -0400

    feat(m26): export identity authorization API

--- app/identity/__init__.py
+++ app/identity/__init__.py
@@ -1,13 +1,17 @@
-"""Durable user identity primitives for M26."""
+"""Durable user identity and authorization primitives for M26."""
 
+from .authorization import RunAccessDeniedError, RunAuthorizationService
 from .models import UserRecord, UserRole
 from .repository import InMemoryUserRepository, SQLiteUserRepository, UserNotFoundError
-from .service import IdentityService, IdentityServiceError
+from .service import AuthenticatedIdentity, IdentityService, IdentityServiceError
 
 __all__ = [
+    "AuthenticatedIdentity",
     "IdentityService",
     "IdentityServiceError",
     "InMemoryUserRepository",
+    "RunAccessDeniedError",
+    "RunAuthorizationService",
     "SQLiteUserRepository",
     "UserNotFoundError",
     "UserRecord",
