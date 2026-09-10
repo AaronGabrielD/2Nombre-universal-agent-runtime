204356db3369de36c35b381947fffc702a2ec4d5
AaronGabrielD <aarongdaishonin@gmail.com>
2026-09-10 12:52:28 -0400

    M41: expose recovery coordinator boundary

--- app/recovery/__init__.py
+++ app/recovery/__init__.py
@@ -1,4 +1,5 @@
 """Controlled post-restart recovery services."""
+from .coordinator import RecoveryCoordinator, RecoveryCoordinatorError, RecoveryPlan
 from .models import RecoveryAction, RecoveryCheckpoint
 from .resume import RecoveryResumeError, RecoveryResumeResult, RecoveryResumeService
 from .service import RecoveryService, RecoveryServiceError
@@ -6,6 +7,9 @@ from .service import RecoveryService, RecoveryServiceError
 __all__ = [
     "RecoveryAction",
     "RecoveryCheckpoint",
+    "RecoveryCoordinator",
+    "RecoveryCoordinatorError",
+    "RecoveryPlan",
     "RecoveryResumeError",
     "RecoveryResumeResult",
     "RecoveryResumeService",
